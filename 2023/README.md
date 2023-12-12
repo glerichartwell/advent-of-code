3fc5745edb8e56d57d1e2b4fa61467af36d856df
glerichartwell <75506480+glerichartwell@users.noreply.github.com>
2023-12-12 15:37:16 -0500

    Day 2 Solutions
    Also, update README to use C++20

--- 2023/README.md
+++ 2023/README.md
@@ -1,6 +1,6 @@
 # Advent of Code 2023 #
 
-This is my first year participating in the Advent of Code. I've wanted to join in on the fun since I heard about it while I was still in university but I could never find the time to participate. This year I will be using these challenges to make sure my programming skills stay sharp as well as branch into languages I don't have much experience in. Most of my solutions will be in C++ as I am trying to learn and get better with this language in particular.
+This is my first year participating in the Advent of Code. I've wanted to join in on the fun since I heard about it while I was still in university but I could never find the time to participate. This year I will be using these challenges to make sure my programming skills stay sharp as well as branch into languages I don't have much experience in. Most of my solutions will be in C++20 as I am trying to learn and get better with this language in particular.
 
 If you're also interested in participating in Advent of Code you can find it here: 
 https://adventofcode.com/
@@ -8,10 +8,10 @@ https://adventofcode.com/
 
 All of my solutions are easiest to run by using the g++ compiler at the command line as so:
 
-`g++ <solution-file> -o <output-file> && ./<output-file>`
+`g++ -std=c++2a <solution-file> -o <output-file> && ./<output-file>`
 
 e.g.
 
-`g++ day1.cc -o day1.out && ./day1.out`
+`g++ -std=c++2a day1.cc -o day1.out && ./day1.out`
 
 Make sure you are in the proper directory before trying to compile.
--- 2023/day2/day2.cc
+++ 2023/day2/day2.cc
@@ -1,18 +1,19 @@
 /**
- * @file day1.cc
+ * @file day2.cc
  * @author Eric Hartwell
  * @date 2023
  * 
- * This file contains the solutions for Day 1
+ * This file contains the solutions for Day 2
  * 
 */
-    
+
 #include <cctype>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <map>
 #include <sstream>
+#include <tuple>
 
 using namespace std;
 namespace game {
@@ -21,9 +22,22 @@ namespace game {
     const int POSSIBLE = 1;
     const int IMPOSSIBLE = 0;
     
+    tuple<int, string> extractGameData(string game) {
+        int colonIdx = game.find(':');
+
+        string gameData = game.substr(0, colonIdx);
+        int gameID = stoi(gameData.erase(0, 5)); // extract game ID
+        
+        string rounds = game.substr(colonIdx + 1); // extract rounds
+
+        return {gameID, rounds};
+    }
+
     int playRound(string round) {
+        
         stringstream roundStream(round);
         string cubes;
+        
         while (getline(roundStream, cubes, ',')) {
             string numString;
             string color;
@@ -33,17 +47,58 @@ namespace game {
                 return IMPOSSIBLE;
 
         }
+
         return POSSIBLE;
     }
 
     int playGame(string rounds ) {
+        
         stringstream gameStream(rounds);
         string round;
+        
         while (getline(gameStream, round, ';')) {
             if (playRound(round) == IMPOSSIBLE)
                 return IMPOSSIBLE;
         }
+
         return POSSIBLE;
+    }
+
+    tuple<int, int, int> updateFewestCubes(string round, tuple<int, int, int> maxes) {
+        stringstream roundStream(round);
+        string cubes;
+        auto [blueMax, greenMax, redMax] = maxes;
+        while (getline(roundStream, cubes, ',')) {
+            
+            string numString;
+            string color;
+            stringstream(cubes) >> numString >> color;
+            int numCubes = stoi(numString);
+
+            if (color == "blue") {
+                blueMax = numCubes > blueMax ? numCubes : blueMax;
+            } else if (color == "green") {
+                greenMax = numCubes > greenMax ? numCubes : greenMax;
+            } else if (color == "red") {
+                redMax = numCubes > redMax ? numCubes : redMax;
+            }
+
+        }
+
+        return {blueMax, greenMax, redMax};
+    }
+
+    int calcSetPower(string rounds) {
+        stringstream gameStream(rounds);
+        string round;
+        tuple<int, int, int> maxes{0, 0, 0};
+        int setPower = 0;
+        while (getline(gameStream, round, ';')) {
+            maxes = updateFewestCubes(round, maxes);
+        }
+        auto [blue, green, red] = maxes;
+        setPower = blue * green * red;
+        return setPower;
     }
 }
 
@@ -55,27 +110,35 @@ int partOne() {
     int idSum = 0;
     string game;
     while (getline(inputFile, game)) {
-        
-        int colonIdx = game.find(':');
-
-        string gameData = game.substr(0, colonIdx);
-        int gameID = stoi(gameData.erase(0, 5)); // extract game ID
-        
-        string rounds = game.substr(colonIdx + 1); // extract rounds
-
+        auto [gameID, rounds] = game::extractGameData(game);
         if (game::playGame(rounds) == game::POSSIBLE) 
             idSum += gameID;
-
-        
     }
+
     inputFile.close();
     return idSum;
+}
+
+int partTwo() {
+    ifstream inputFile;
+    inputFile.open("input.txt");
+
+    int setPowerSum = 0;
+    string game;
+    while (getline(inputFile, game)) { 
+        auto [gameID, rounds] = game::extractGameData(game);
+        setPowerSum += game::calcSetPower(rounds);
+    }
+
+    inputFile.close();
+    return setPowerSum;
 }
 
 
 int main() {
     
     cout << "Day 2 Part 1: " << partOne() << endl;
+    cout << "Day 2 Part 2: " << partTwo() << endl;
 
     return 0;
 }
